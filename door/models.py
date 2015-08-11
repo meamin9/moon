e7b2c4501ed859903ef3173881e218450b6dbc18
meamin9 <meamin_n@163.com>
2015-08-11 23:44:55 +0800

    view

--- door/models.py
+++ door/models.py
@@ -2,11 +2,24 @@ from django.db import models
 
 
 # Create your models here.
+class TimeLog(models.Model):
+    ctime = models.DateTimeField()
+    mtime = models.DateTimeField()
+    atime = models.DateTimeField()
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=32)
+    title = models.CharField(max_length=32, blank=True)
+    time = TimeLog()
+    exp = models.BigIntegerField(default=0)
+
+
 class Text(models.Model):
-    author = models.CharField(max_length=32)
+    author = models.ForeignKey(Role)
     title = models.CharField(max_length=128)
     content = models.TextField()
-    ctime = models.DateTimeField()
+    time = TimeLog()
 
     def __unicode__(self):
         return self.title
--- door/views.py
+++ door/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def test_view(request):
+    return photo_view(request)
+    # return render(request, 'common/nav.html')
+    # return render(request, 'picture/view.html')
+
+
+def photo_view(request):
+    img_list = []
+    for i in xrange(13):
+        img_list.append('images/album/MishimaKurune/%03d.jpg' % (i+1))
+    return render(request, 'picture/photos.html', {'img_list': img_list})
