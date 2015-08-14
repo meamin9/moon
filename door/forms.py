dc39da40fb260b573fe613dce4e22d3f05e593ee
meamin9 <meamin_n@163.com>
2015-08-14 18:48:18 +0800

    regiter

--- door/forms.py
+++ door/forms.py
@@ -1,12 +1,14 @@
+# -*- coding:utf-8 -*-
 __author__ = 'meamin9'
 
 from django import forms
 
 
 class RegisterInfo(forms.Form):
-    email = forms.EmailField(label='Email', widget=forms.TextInput(attrs={'class': 'form-control',
-                                                                          'placeholder': 'Email'}))
-    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control',
-                                                                                   'placeholder': 'Password'}))
+    username = forms.CharField(label='用户名', max_length=32, min_length=3,
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'User name'}))
+    password = forms.CharField(label='密码',
+                               widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    email = forms.EmailField(label='Email',
+                             widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
     # repassword = forms.CharField(label='confirm', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    # name = forms.CharField(max_length=32, min_length=3, widget=forms.TextInput(attrs={'class': 'form-control'}))
